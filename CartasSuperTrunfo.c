58c605bf0b7c4621ae0dc01da31b92be17b7934c
Alysson-de-S-Ferreira <alyssonjf07@gmail.com>
2025-11-26 14:45:42 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,68 +9,123 @@ int main() {
       
     //Dados da Primeira Cidade
     //Variáveis para uso
-//Primeiro estado
+    printf("Dados da Primeira Cidade \n");
 char estado1[15];
-
-//Código da Carta
 char código[4];
-
-//Nome da Cidade
 char cidade[15];
-
-//População da cidade
 int população;
-
-//Área da Cidade
 int Área;
-
-//PIB da cidade
 float PIB;
-
-//Pontos Turisticos da cidade
 int Pontos;
-
-
-//Entradas de dados no programa
-//Para o usuátio completar-
+float PPC;
+float densidade;
 
 //Nome do Estado
 printf("Nome do Estado: \n");
-scanf("%14s", estado1);
+scanf("%s", estado1);
 
 //Código da Carta
 printf("Digite o código da carta: \n");
-scanf(" %s", código);
+scanf(" %3s", código);
 
 //Nome da Cidade
 printf("Nome da Cidade: \n");
-scanf("%s", cidade);
+scanf(" %s", cidade);
 
 //População
 printf("Populção: \n");
-scanf("%d", &população);
+scanf(" %d", &população);
 
 //Área da Cidade
 printf("Área: \n");
-scanf("%d", &Área);
+scanf(" %d", &Área);
 
 //PIB
 printf("PIB: \n");
-scanf("%.2f", &PIB);
+scanf(" %f", &PIB);
 
 //Numero de Pontos Turisticos
 printf("Pontos Turisticos: \n");
 scanf("%d", &Pontos);
 
-//Visualização dos dados
-//Linha após linha
+PPC = (float) (PIB / população);
+densidade = (float) (população / Área);
+
+
+
+//Dados da Segunda Cidade
+printf("Segunda Cidade: \n");
+
+//Váriaveis
+char estado2[15];
+char código2[4];
+char cidade2[15];
+int população2;
+int Área2;
+float PIB2;
+int Pontos2;
+float PPC2;
+float densidade2;
+
+//Nome do Estado
+printf("Nome do Estado: \n");
+scanf("%s", estado2);
+
+//Código da Carta
+printf("Digite o código da carta: \n");
+scanf(" %3s", código2);
+
+//Nome da Cidade
+printf("Nome da Cidade: \n");
+scanf(" %s", cidade2);
+
+//População
+printf("Populção: \n");
+scanf(" %d", &população2);
+
+//Área da Cidade
+printf("Área: \n");
+scanf(" %d", &Área2);
+
+//PIB
+printf("PIB: \n");
+scanf(" %f", &PIB2);
+
+//Numero de Pontos Turisticos
+printf("Pontos Turisticos: \n");
+scanf("%d", &Pontos2);
+
+PPC2 = (float) (PIB2 / população2);
+densidade2 = (float) (população2 / Área2);
+
+//Saída de Dados das cidades
+
+//Saída de Dados Primeira Cidade
+//Amostragem na Tela
+printf("Dados da Primeira Cidade -\n");
 printf("Estado: %s\n", estado1);
 printf("Código: %s\n", código);
 printf("Nome da Cidade %s\n", cidade);
 printf("População: %d\n", população);
 printf("Área: %d\n", Área);
-printf("PIB: %f\n", PIB);
+printf("PIB: %.2f\n", PIB);
 printf("Pontos Turisticos: %d\n", Pontos);
+printf("PIB Per Capita: %.2f\n", PPC);
+printf("Densidade Demográfica: %.2f\n", densidade);
+
+//Saída de Dados da Segunda Cidade
+//Amostragem na Tela
+printf("Dados da Segunda Cidade -\n");
+printf("Estado: %s\n", estado2);
+printf("Código: %s\n", código2);
+printf("Nome da Cidade %s\n", cidade2);
+printf("População: %d\n", população2);
+printf("Área: %d\n", Área2);
+printf("PIB: %.2f\n", PIB2);
+printf("Pontos Turisticos: %d\n", Pontos2);
+printf("PIB Per Capita: %.2f\n", PPC2);
+printf("Densidade Demográfica: %.2f\n", densidade2);
+
 
 
 return 0;
